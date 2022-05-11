a028997b60023d6e68c7405d10a05080782b8034
HanCiHu <kinm1596@daum.net>
2022-05-11 16:19:39 +0900

    ✨ props 구조 분해 할당

--- Practice02/src/app.tsx
+++ Practice02/src/app.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
-interface AppProps {
-  propsString: string
-};
+/*
 
 const App = (props: AppProps): JSX.Element => {
   return (
     <div>
       {'Hello, World!'}
       {props.propsString}
+    </div>
+  )
+}
+*/
+
+interface AppProps {
+  propsString: string
+};
+
+const App = ({ propsString }: AppProps): JSX.Element => {
+  return (
+    <div>
+      {'Hello, World!'}
+      {propsString}
     </div>
   )
 }
