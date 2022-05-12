2fb1f2f9403835ef23f9bb0ae6184319e5cb6400
HanCiHu <kinm1596@daum.net>
2022-05-12 22:19:52 +0900

    ✨ using useState

--- Practice03/src/app.tsx
+++ Practice03/src/app.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = (): JSX.Element => {
+  const [num, setNum] = useState<number>(0);
+
   return (
     <div>
-      {'Hello, World!'}
+      <p>{num}</p>
+      <button onClick={()=> setNum(num + 1)}>숫자 늘리기</button>
     </div>
   )
 }
