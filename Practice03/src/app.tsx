c18625b28021b2c10b38a4e37aa64886390aeaf4
HanCiHu <kinm1596@daum.net>
2022-05-31 15:13:13 +0900

    ✨ useState batching

--- Practice03/src/app.tsx
+++ Practice03/src/app.tsx
@@ -3,12 +3,17 @@ import React, { useState } from 'react';
 const App = (): JSX.Element => {
   const [num, setNum] = useState<number>(0);
 
+  const addNum = (): void => {
+    setNum(num + 1);
+    setNum(num + 1);
+  };
+
   return (
     <div>
       <p>{num}</p>
-      <button onClick={()=> setNum(num + 1)}>숫자 늘리기</button>
+      <button onClick={addNum}>숫자 늘리기</button>
     </div>
-  )
+  );
 }
 
 export default App;
