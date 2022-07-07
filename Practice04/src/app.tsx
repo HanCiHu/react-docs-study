ea02ca6772ad9ec0dbb334f4c9a126777f3a1c8d
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-07-07 16:42:36 +0900

    ✨ practice hooks rules

--- Practice04/src/app.tsx
+++ Practice04/src/app.tsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = (): JSX.Element => {
   const [num, setNum] = useState<number>(0);
 
-  const a = () => {
-    setNum((n) => n + 1);
+  if (num == 0) {
+    useEffect(() => {
+      setNum1(num1 + 1);
+    }, [num]);
   }
 
-  useEffect(() => {
-    const strDiv = document.getElementById('strDiv');
-    console.log(num);
-    strDiv?.addEventListener('click', a);
-    return () => {
-      strDiv?.removeEventListener('click',a);
-    }
-  });
+  const [num1, setNum1] = useState<number>(1);
+
+  const add = () => {
+    setNum(num + 1);
+    setNum1(num1 + 1);
+  };
 
   return (
     <div>
-      <div id='strDiv'>{"str"}</div>
+      <div id="strDiv">{"str"}</div>
       <p>{num}</p>
-      <button onClick={() => setNum(num + 1)}>숫자 늘리기</button>
+      <p>{num1}</p>
+      <button onClick={add}>숫자 늘리기</button>
     </div>
   );
 };
